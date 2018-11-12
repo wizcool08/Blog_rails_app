91f4c1b6e32ec8b059ac8c078967c7294aec3d9c
Kim Yong <kimyong.lim.2017@gmail.com>
2018-11-12 11:05:07 +0800

    setup backend user password authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_085906) do
+ActiveRecord::Schema.define(version: 2018_11_12_025646) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_11_09_085906) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
