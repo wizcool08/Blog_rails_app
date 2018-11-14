5ce94464245ee25d09df8e94cd4cc1c7a7548485
Kim Yong <kimyong.lim.2017@gmail.com>
2018-11-14 23:54:38 +0800

    create add and edit  user signup account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-root "pages#home"
-get  'about', to: "pages#about"
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
-resources :articles
+  resources :articles
 
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
