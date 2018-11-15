535c64e7e1596baaa59a5f7850bc169608c04750
Kim Yong <kimyong.lim.2017@gmail.com>
2018-11-16 00:20:15 +0800

    Add pagination to Article and UserViews

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %I[edit update show destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def new
